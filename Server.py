d1e87c139b79f3bc06bcaf3f8b313b9cb02914ca
CallMeURUBONZ <69807802+CallMeURUBONZ@users.noreply.github.com>
2020-08-31 17:35:06 -0400

    Added Code v1

--- Server.py
+++ Server.py
@@ -1,1 +1,23 @@
+Username = input("Enter Username: ")
+from datetime import datetime
+import os
+currentcwd = os.getcwd()
+now = datetime.now()
+import sys
+from flask import Flask, render_template
+testvar = "hello"
+current_time = now.strftime("%H:%M:%S")
+
+
+
+
+
+while True:
+    logfile = open("Log.txt", "a+")
+    for x in range(1):
+        Message = input("Message: ")
+        logfile.writelines(f'{current_time}|||{Username}: {Message}\n')
+        logfile.close()
+
+
 
